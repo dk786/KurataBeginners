3b7fbaf33af5e6c7ccceb8a9aed40bd3357b31ab
dk786 <dk786@hotmail.com>
2019-08-02 15:36:04 +0100

    Kurata tutorial from pluralsight first commit after installing bootstrap and jquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+/* Meta data to provide template data.  needs the @component decorator to be identified as the components metadata*/
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-pm-root', // the tag that is used in the template to replace with this component
+  templateUrl: './app.component.html',  // the relative path to the html view template for this component
+  styleUrls: ['./app.component.css']  // the relative path to the css styles template for this component
 })
+
+/*This is the root component class. classes in Typescript use export as the equivalent of the public access */
 export class AppComponent {
-  title = 'KurataBeginners';
+  title = 'Kurata Beginners Course';
 }
